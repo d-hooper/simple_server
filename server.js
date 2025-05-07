c18fa1a11566f1c773593009fb82f0354ce6505f
d-hooper <dallinhoop3@gmail.com>
2025-05-07 17:29:40 -0600

    send html

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Having fun with Docker and Node 🐳\n');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Docker Test</title>
+</head>
+<body>
+<h1>Having fun with Docker 🐳</h1>
+</body>
+</html>`);
 });
 
 server.listen(PORT, () => {
