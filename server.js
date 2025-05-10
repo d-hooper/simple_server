2abe5a1fd836d3a31e6f4a1ed06b2be765a138a5
d-hooper <dallinhoop3@gmail.com>
2025-05-09 22:16:28 -0600

    server req update

--- server.js
+++ server.js
@@ -4,18 +4,26 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end(`<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Docker Test</title>
-</head>
-<body>
-<h1>Having fun with Docker 🐳</h1>
-</body>
-</html>`);
+  console.log("incoming request")
+  try {
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(`<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Docker Test</title>
+  </head>
+  <body>
+  <h1>Having fun with Docker 🐳</h1>
+  </body>
+  </html>`)
+    res.end();
+  } catch (error) {
+    console.error("[ERROR IN RESPONSE]", error)
+    res.end()
+  }
 });
 
 server.listen(PORT, () => {
